493577fd2f154a3b8ada60ab5072d7cd60dc4b23
Andrey Karev <zirockz@mail.ru>
2020-05-02 12:37:16 +0300

    Player Connection Bugs Fixed

--- api/socket.io/socket-handler.js
+++ api/socket.io/socket-handler.js
@@ -5,21 +5,30 @@ let allClients = [];
 
 
 io.sockets.on('connection', (socket) => {
-
-    allClients.push(socket.id);
-
-    console.log("New Player connected! Players Array:")
-    for (let soc of allClients) {
-        console.log(soc, allClients.indexOf(soc))
-    }
-
-    socket.on('move player', (action) => {
-        io.sockets.emit('move player', action);
-    })
-
-    io.sockets.emit('new player connected', socket.id);
+    
     socket.emit('player connection', socket.id);
+    socket.on('player connected', (player) => {          
+        console.log("array", allClients)     
+        socket.emit('add all players', allClients);
+        allClients.push(player); 
+        socket.broadcast.emit('new player connected', player);
+      //  console.log("New Player connected! Players Array:")
+     //   console.log(allClients)
+    })     
+    
+    socket.on('move player', (data) => {
+        console.log(data)
+        let playerIndex = allClients.findIndex(el => el.socketId === data.socketId);
+        if (playerIndex !== -1) {
+            if (allClients[playerIndex].direction !== data.direction) {
+                allClients[playerIndex].direction = data.direction;
+                allClients[playerIndex].xPos -= 50 * data.direction;
+            }
+            allClients[playerIndex].xPos += 10 * data.direction;
 
+            io.sockets.emit('move player', {xPos: allClients[playerIndex].xPos, socketId: data.socketId, direction: data.direction });
+        }            
+    })
 
     socket.on('disconnect', () => {
         console.log('Got disconnect!');
